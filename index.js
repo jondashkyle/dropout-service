8ec25c3d4f57f8cb6b353007a893f0510275d5b8
jondashkyle <contact@jon-kyle.com>
2017-10-28 11:56:25 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+var getPort = require('getport')
+var merry = require('merry')
+
+var env = { PORT: 8080 }
+var app = merry({ env: env })
+
+app.route('GET', '/', function (req, res, ctx) {
+  ctx.log.info('oh hey, a request here')
+  ctx.send(200, { cute: 'butts' })
+})
+
+app.route('default', function (req, res, ctx) {
+  ctx.log.info('Route doesnt exist')
+  ctx.send(404, { message: 'nada butts here' })
+})
+
+app.listen(app.env.PORT)
+console.log('Listening at https://localhost:' + app.env.PORT)
