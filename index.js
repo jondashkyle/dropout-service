97ef310f4a40069bc0265b4fafc4c7a8b35eb3cc
jondashkyle <contact@jon-kyle.com>
2017-10-28 15:38:13 -0700

    saving works

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+var parseBody = require('parse-body')
+var dropout = require('dropout')
 var getPort = require('getport')
 var merry = require('merry')
 var http = require('http')
@@ -5,9 +7,22 @@ var http = require('http')
 var env = { PORT: 8080 }
 var app = merry({ env: env })
 
-app.route('GET', '/', function (req, res, ctx) {
-  ctx.log.info('oh hey, a request here')
-  ctx.send(200, { cute: 'butts' }, headers())
+app.route('POST', '/', function (req, res, ctx) {
+  // if (!ctx.params.url) return ctx.send(405, { message: 'No URL provided' }, headers())
+  parseBody(req, 1e6, function (err, body) {
+    if (err) ctx.send(500, { message: err.message }, headers())
+    ctx.log.info('oh hey, a request here')
+    try {
+      dropout.get(body.url, { }, function (err, data) {
+        ctx.send(200, data, headers())
+      })
+    } catch (err) {
+      console.log(err)
+      ctx.send(500, { message: err.message }, headers())
+    }
+  })
+
+
 })
 
 app.route('default', function (req, res, ctx) {
