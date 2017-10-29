13c3c75cf5754ba0bd3a18b48c4986e820fb9662
jondashkyle <contact@jon-kyle.com>
2017-10-28 23:10:21 -0700

    cleanup:

--- index.js
+++ index.js
@@ -8,20 +8,20 @@ var env = { PORT: 8080 }
 var app = merry({ env: env })
 
 app.route('POST', '/', function (req, res, ctx) {
+  console.log(req)
   parseBody(req, 1e6, function (err, body) {
     if (err) ctx.send(500, { message: err.message }, headers())
-    ctx.log.info('oh hey, a request here')
+    ctx.log.info('request: ' + body.url)
     try {
       dropoutGet(body.url, { }, function (err, data) {
+        ctx.log.info('success: ' + body.url)
         ctx.send(200, data, headers())
       })
     } catch (err) {
-      console.log(err)
+      ctx.log.error('error: ' + body.url)
       ctx.send(500, { message: err.message }, headers())
     }
   })
-
-
 })
 
 app.route('default', function (req, res, ctx) {
